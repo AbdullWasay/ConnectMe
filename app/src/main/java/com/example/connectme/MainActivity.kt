b16431a33f302e901c242cc2af7388eca1717cc7
AbdulWasay <abdul.wasay308@gmail.com>
2025-02-10 19:43:51 +0500

    Screen2

--- app/src/main/java/com/example/connectme/MainActivity.kt
+++ app/src/main/java/com/example/connectme/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.connectme
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Show Screen1 for 3 seconds
+        setContentView(R.layout.activity_main) // Screen 1 layout
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            showScreen2() // Navigate to Screen 2
+        }, 3000)
+    }
+
+    private fun showScreen2() {
+        setContentView(R.layout.screen2) // Screen 2 layout
+
+        val registerText = findViewById<TextView>(R.id.secondText)
+        registerText.setOnClickListener {
+            showScreen3()
+        }
+    }
+
+    private fun showScreen3() {
+        setContentView(R.layout.screen3) // Screen 3 layout
+
+        val loginText = findViewById<TextView>(R.id.secondText)
+        loginText.setOnClickListener {
+            showScreen2()
         }
     }
 }
